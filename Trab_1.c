260af453f22574b00b667a49397f88bac21390b6
carlossecato <carlos.secato@yahoo.com.br>
2016-09-16 13:44:38 -0300

    Add files via upload

--- Trab_1.c
+++ Trab_1.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 
 
-
+int menu ();
+int menu2();
+int abrir_arq(FILE *, int *, int *);
+int bubbleSort(int *,int);
+int insertionSort (int*,int );
+int mergeSort(int *, int, int,int );
 
 
 int main (void){
 	
-	int op,op2,op3;
-	
-	FILE *arq1, *arq2, *arq3;
+	int op,op2,op3, *size=NULL;
+	int *p=NULL;
+	int tam=0, posicaoInicio, posicaoFim;
+	FILE *arq1;
+	FILE *arq2;
+	FILE *arq3;
 	
 	
 	op=menu();
@@ -19,24 +27,35 @@ int main (void){
 		switch (op){
 			case 1: op3=menu2();
 					system("cls");
-						do{
+						
 						
 						switch (op3){
-							case 1: //bubbleSort();
+							case 1: p=abrir_arq(arq2,p,&size);
+									bubbleSort(p,size);
 								break;
-							case 2: insertionSort(arq2);
+							case 2: p=abrir_arq(arq2,p,&size);
+									insertionSort(p,size);
 								break;
-							case 3: //mergeSort();
+							case 3: 
+									p=abrir_arq(arq2,p,&size);
+									posicaoInicio=0;
+									posicaoFim = (int)size-1;
+									mergeSort(p,posicaoInicio, posicaoFim,size);
 								break;
 							case 4: op=menu();
 									system("cls");
 								break;
 							default: printf("Opcao invalida!!");
 						}
-					}while(op3!=4);
+			
 				 
 				break;
-			case 2: 
+			case 2: p=abrir_arq(arq2,p,&size);
+					bubbleSort(p,size);
+					p=abrir_arq(arq2,p,&size);
+					insertionSort(p,size);
+					p=abrir_arq(arq2,p,&size);
+					mergeSort(p,posicaoInicio, posicaoFim,size);
 				break;
 			case 3: 
 				return EXIT_SUCCESS;
@@ -67,33 +86,182 @@ int menu2(){
 	return opcao;
 }
 
-void insertionSort (FILE *arq2){
-	int *p;
-	int i,tam=100000;
+int abrir_arq(FILE *arq2, int *p, int *size){
+	int i;	
 	char filename[30];
-	printf("Digite o nome do arq: ");
-	fflush(stdin);
-	scanf("%s", filename);
-	printf("%s",filename);
 	
-	if(!(arq2=fopen(filename,"r"))){
-		printf("\nERRO na leitura do arquivo");
-		exit(0);
+	//printf("Digite o nome do arq: ");
+	//scanf("%s", filename);
+	
+	arq2 = fopen("teste2.txt","r");
+	if(arq2==NULL){
+	
+		printf("ERRO");
+		exit (1);
 	}
 	else{
-		printf("ok!");
-	}
-	
-	if(p=(int*)calloc,(tam*sizeof(int)==NULL)){
-		printf("Erro na alocacao");
-		exit(0);
+		printf("Arquivo Carregado\n");
 	}
 	
 	i=0;
-	while(!feof(arq2)){
+	while(1){
+		p=(int*)realloc(p,++*size*sizeof(int));
+		fscanf(arq2,"%d", &p[i]);
+		if(feof(arq2)){
+			break;
+		}
+		i++;
+	}
 	
-	fscanf(arq2,"%d",&p[i]);
-	i++;
+	printf("OK");
+	return p;
 }
+int bubbleSort(int *p, int size){
+	FILE *arquivo;
+	int i, j, aux; 
+	int compara=0;
+	int movimenta=0;
+	
+	for(i=size-1; i >= 1; i--){  
+        for(j=0; j < i ; j++){
+        	compara++;
+            if(p[j]>p[j+1]){
+            	
+                aux = p[j];
+                p[j] = p[j+1];
+                p[j+1] = aux;
+                movimenta++;
+            }
+        }
+    }
+	    
+    printf("\n");
+	arquivo = fopen ("SaidaBubbleSort.txt","w");
+	if(!arquivo){
+		printf("Erro abrir arq saida");
+		exit(1);
+	}
+	fprintf(arquivo,"Esse algoritmo necessitou de %d comparacoes e %d interacoes\n",compara,movimenta);
+	for(i = 0; i < size-1; i++){
+        fprintf(arquivo,"%d\n",p[i]);
+        
+    }
+    
+    fclose(arquivo);
+	
+	return p;
+}
+
+int insertionSort (int *p, int size){
+	FILE *arquivo;
+	int i, j, aux;
+	
+	int compara=0;
+	int movimenta=0;
+	
+
+	for(j=1;j<size;j++){
+		aux=p[j];
+		i=j-1;
+		compara++;
+		while (i>=0 && p[i]>aux){
+			p[i+1]=p[i];
+			i=i-1;
+			movimenta++;
+		}
+			p[i+1]=aux;
+	}
+
+	printf("\n");
+	arquivo = fopen ("SaidaInsertionSort.txt","w");
+	if(!arquivo){
+		printf("Erro abrir arq saida");
+		exit(1);
+	}
+	fprintf(arquivo,"Esse algoritmo necessitou de %d comparacoes e %d interacoes\n",compara,movimenta);
+	for(i=0;i<size-1;i++){
+		fprintf(arquivo,"%d\n",p[i]);
+	}	
+	
+	fclose(arquivo);
+	
+	return p;
+}
+
+int mergeSort(int *p, int posicaoInicio, int posicaoFim, int size){
+	int i, j, k, metadeTamanho, *vetorTemp;
+	FILE *arquivo;
+	int compara=0;
+	int movimenta=0;
+	 
+	
+if(posicaoInicio == posicaoFim) return 1;
+    // ordenacao recursiva das duas metades
+    metadeTamanho = (posicaoInicio + posicaoFim ) / 2;
+    mergeSort(p, posicaoInicio, metadeTamanho,size);
+    mergeSort(p, metadeTamanho + 1, posicaoFim,size);
+
+
+    // intercalacao no vetor temporario t
+    i = posicaoInicio;
+    j = metadeTamanho + 1;
+    k = 0;
+    vetorTemp = (int *) malloc(sizeof(int) * (posicaoFim - posicaoInicio + 1));
+
+    while(i < metadeTamanho + 1 || j  < posicaoFim + 1) {
+        if (i == metadeTamanho + 1 ) { // i passou do final da primeira metade, pegar v[j]
+            vetorTemp[k] = p[j];
+            j++;
+            k++;
+            compara++;
+            movimenta++;
+        }
+        else {
+            if (j == posicaoFim + 1) { // j passou do final da segunda metade, pegar v[i]
+                vetorTemp[k] = p[i];
+                i++;
+                k++;
+                compara++;
+            	movimenta++;
+            }
+            else {
+                if (p[i] < p[j]) {
+                    vetorTemp[k] = p[i];
+                    i++;
+                    k++;
+                    compara++;
+            		movimenta++;
+                }
+                else {
+                    vetorTemp[k] = p[j];
+                    j++;
+                    k++;
+                    compara++;
+            		movimenta++;
+                }
+            }
+        }
+
+    }
+    // copia vetor intercalado para o vetor original
+    for(i = posicaoInicio; i <= posicaoFim; i++) {
+        p[i] = vetorTemp[i - posicaoInicio];
+        movimenta++;
+    }
+    free(vetorTemp);
+    
+	arquivo = fopen ("SaidaMergeSort.txt","w");
+	if(!arquivo){
+		printf("Erro abrir arq saida");
+		exit(1);
+	}
+	fprintf(arquivo,"Esse algoritmo necessitou de %d comparacoes e %d interacoes\n",compara,movimenta);
+	for(i = 0; i < size-1; i++){
+        fprintf(arquivo,"%d\n",p[i]);
+    }
+    
+    fclose(arquivo);
+	
+	return p;
 }
 
